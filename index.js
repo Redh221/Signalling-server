2b7473b3593027a8e48b9f5d1877f869db053116
Redh221 <denis.markin.95@gmail.com>
2024-11-26 18:57:00 +0400

    removing_wrong_command

--- index.js
+++ index.js
@@ -9,7 +9,3 @@ wss.init(webSocketPort);
 app.listen(HTTP_Port, () => {
   console.log("Server listening on port ", HTTP_Port);
 });
-
-server.listen(8090, () => {
-  console.log("Secure WebSocket server running on wss://192.168.100.2:8090");
-});
