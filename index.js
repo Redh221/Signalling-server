9392467ba6d7746cfc0ded9c08b0bc09a70e7e68
Grack <grackbox@gmail.com>
2024-11-27 17:18:52 +0500

    remove https

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-const https = require("https");
 const express = require("express");
 const WebSocket = require("ws");
 const wss = require("./wss");
@@ -11,23 +9,18 @@ const webSocketPort = 8090;
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
-// Пути к SSL-сертификатам
-const certPath = "./path/to/cert.pem"; // Заменить на путь к твоему сертификату
-const keyPath = "./path/to/key.pem"; // Заменить на путь к твоему ключу
 
-// Чтение сертификатов
-const cert = fs.readFileSync(certPath);
-const key = fs.readFileSync(keyPath);
+const wssServer = new WebSocket.Server({ noServer: true });
 
-// Создание HTTPS сервера с сертификатами
-const server = https.createServer({ key, cert }, app);
-
-// Инициализация WebSocket сервера на основе HTTPS сервера
-const wssServer = new WebSocket.Server({ server });
-
-// Инициализация WebSocket серверной логики
 wss.init(wssServer);
 
-server.listen(HTTP_Port, "0.0.0.0", () => {
-  console.log(`HTTPS сервер работает на https://0.0.0.0:${HTTP_Port}/`);
+// В HTTP режиме не нужно устанавливать сертификаты, просто запускаем сервер
+const server = app.listen(HTTP_Port, () => {
+  console.log(`Express сервер работает на http://localhost:${HTTP_Port}/`);
+});
+
+server.on("upgrade", (request, socket, head) => {
+  wssServer.handleUpgrade(request, socket, head, (ws) => {
+    wssServer.emit("connection", ws, request);
+  });
 });
