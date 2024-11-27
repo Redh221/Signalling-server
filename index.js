195459b0978a11ec386c15fccf12e17eb2419fef
Grack <grackbox@gmail.com>
2024-11-27 19:01:04 +0500

    dubina

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ const wss = require("./wss");
 
 const app = express();
 
-const HTTP_Port = 4000;
-const webSocketPort = 8090;
+const HTTP_Port = 4000; // HTTP сервер на 4000 порту
+const webSocketPort = 8090; // WebSocket сервер на 8090 порту
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-const wssServer = new WebSocket.Server({ noServer: true });
+// Настроим WebSocket сервер на порту 8090
+const wssServer = new WebSocket.Server({ port: webSocketPort });
 
+// Инициализация WebSocket
 wss.init(wssServer);
 
-// В HTTP режиме не нужно устанавливать сертификаты, просто запускаем сервер
+// Запуск HTTP сервера на порту 4000
 const server = app.listen(HTTP_Port, () => {
   console.log(`Express сервер работает на http://localhost:${HTTP_Port}/`);
 });
 
+// Важно: для WebSocket серверов через upgrade, нужно обработать событие upgrade
 server.on("upgrade", (request, socket, head) => {
+  // Обработка подключения WebSocket через HTTP сервер
   wssServer.handleUpgrade(request, socket, head, (ws) => {
     wssServer.emit("connection", ws, request);
   });
