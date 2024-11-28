3d85d86f32f3b919e0e3f4d41e85b977282ce590
Denis Markin <116741837+Redh221@users.noreply.github.com>
2024-11-28 18:11:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const WebSocket = require("ws");
 const https = require("https");
 const fs = require("fs");
+const { Server } = require("socket.io");
 const wss = require("./wss");
 
 const app = express();
 
-const HTTP_Port = 4000; // Сервер на порту 4000
+const HTTP_Port = 4000;
 
-// Загрузка SSL сертификатов для HTTPS
 const options = {
   cert: fs.readFileSync(
     "/etc/letsencrypt/live/signal-server.waterhedgehog.com/fullchain.pem"
@@ -18,50 +17,23 @@ const options = {
   ),
 };
 
-// Создаем HTTPS сервер
 const server = https.createServer(options, app);
 
-// Настроим WebSocket сервер через HTTPS (WSS)
-const wssServer = new WebSocket.Server({
-  server, // Указываем, что WebSocket сервер будет использовать тот же сервер
-  path: "/ws", // Путь для WebSocket
+const io = new Server(server, {
+  path: "/ws",
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
 });
 
-// Инициализация WebSocket
-wss.init(wssServer);
-
-// Логирование подключения клиента
-wssServer.on("connection", (ws, req) => {
-  console.log(
-    `[${new Date().toISOString()}] WebSocket подключение от ${
-      req.connection.remoteAddress
-    }`
-  );
-
-  ws.on("message", (message) => {
-    console.log(`Получено сообщение: ${message}`);
-  });
-
-  ws.on("close", () => {
-    console.log(`Подключение закрыто: ${req.connection.remoteAddress}`);
-  });
-
-  ws.on("error", (error) => {
-    console.error("Ошибка WebSocket:", error);
-  });
-});
+wss.init(io);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!"); // Ваш основной роут
+  res.send("Hello, World!");
 });
 
-// Запуск HTTPS сервера
 server.listen(HTTP_Port, () => {
   console.log(`Express сервер работает на https://localhost:${HTTP_Port}/`);
-  console.log("WebSocket сервер работает на wss://localhost:4000/ws/");
-});
-
-// Обработка ошибок на WebSocket сервере
-wssServer.on("error", (error) => {
-  console.error("Ошибка WebSocket сервера:", error);
+  console.log("Socket.io сервер работает на wss://localhost:4000/ws/");
 });
